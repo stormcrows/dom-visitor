9965c4e30f342d1cfd7e6f7f5fe4864ccc4dd12f
stormcrows <stormingcrows@gmail.com>
2017-10-20 13:20:33 +0100

    first commit

--- index.spec.js
+++ index.spec.js
@@ -2,23 +2,32 @@ const DOMVisitor = require("./index");
 const { html, body, div } = require("./fakedom");
 
 describe("removeAttribute", () => {
+  const removeAttribute = (attr, node) =>
+    DOMVisitor(node, node => (node[attr] ? delete node[attr] : void 0), 0);
+
+  const DOM = html([
+    body([
+      div([], [["onclick", () => {}]]),
+      div([div([], [["onclick", () => {}]]), []])
+    ])
+  ]);
+
   it("should remove onclick attributes from all nodes", () => {
     let count = 0;
     const attributeCount = (attr, node) =>
       DOMVisitor(node, node => (count += node[attr] ? 1 : 0));
 
-    const removeAttribute = (attr, node) =>
-      DOMVisitor(node, node => (node[attr] ? delete node[attr] : void 0), 0);
-
-    const DOM = html([
-      body([
-        div([], [["onclick", () => {}]]),
-        div([div([], [["onclick", () => {}]]), []])
-      ])
-    ]);
-
     removeAttribute("onclick", DOM);
 
     expect(attributeCount("onclick", DOM)).toBe(0);
+  });
+
+  it("should be resilient to incorrect values passed as nodes", () => {
+    expect(() => removeAttribute("onclick", [])).not.toThrow();
+    expect(() => removeAttribute("onclick", {})).not.toThrow();
+    expect(() => removeAttribute("onclick", "abc")).not.toThrow();
+    expect(() => removeAttribute("onclick", 1)).not.toThrow();
+    expect(() => removeAttribute("onclick", null)).not.toThrow();
+    expect(() => removeAttribute("onclick", undefined)).not.toThrow();
   });
 });
